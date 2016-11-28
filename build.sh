aad9d3f0e8a63446ba5a4379a31a3d85da1ae409
Thibault Joan (Xilinx) <joan.thibault@ens-rennes.fr>
2016-11-28 17:08:34 +0000

    modify build.sh and CMakeLists.txt

--- build.sh
+++ build.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 # the path to the ComputeCPP package root directory, e.g. /home/user/ComputeCpp-CE-0.1-Linux/
-PACKAGE_ROOT=$1
+
+# How to use build.sh to compile SyclParallelSTL with ComputeCpp ?
+# ./build.sh "path/to/ComputeCpp"
+#
+# How to use build.sh to compile SyclParallelSTL with triSYCL ?
+# ./build.sh --trisycl "path/to/triSYCL" ["path/to/compute"]
+#
+# paths can be relative ones, this script will make them absoulte before to send them to CMake
+#
+# 
+
+
+
+if [ $1=="--trisycl" ]
+then
+	CMAKE_ARGS="$CMAKE_ARGS -DUSE_COMPUTECPP=OFF -DTRISYCL_PACKAGE_ROOT_DIR=$(readlink -f $2)"
+	if [ $3 ]
+	then
+		CMAKE_ARGS="$CMAKE_ARGS -DCOMPUTE_PACKAGE_ROOT_DIR=$(readlink -f $3)"
+	fi
+else
+	CMAKE_ARGS="$CMAKE_ARGS -DUSE_COMPUTECPP=ON -DCOMPUTECPP_PACKAGE_ROOT_DIR=$(readlink -f $1)"
+fi
+NPROC=$(nproc)
 
 function install_gmock  {
   mkdir -p external && pushd external
-  git clone git@github.com:google/googletest.git
+  git clone https://github.com/google/googletest.git
   pushd googletest/googlemock/make && make
   popd 
   popd
@@ -12,12 +35,12 @@ function install_gmock  {
 
 function configure  {
   mkdir -p build && pushd build 
-  cmake .. -DCOMPUTECPP_PACKAGE_ROOT_DIR=$PACKAGE_ROOT  -DPARALLEL_STL_BENCHMARKS=ON
+  cmake .. $CMAKE_ARGS -DPARALLEL_STL_BENCHMARKS=ON
   popd
 }
 
 function mak  {
-  pushd build && make -j32
+  pushd build && make -j$NPROC
   popd
 }
 

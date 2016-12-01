4a25b03523a39ed49963c91877b27d76f39c28b4
Thibault Joan (Xilinx) <joan.thibault@ens-rennes.fr>
2016-12-01 15:30:25 +0000

    simplify build.sh(triSYCL mode) + add examples

--- build.sh
+++ build.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-# the path to the ComputeCPP package root directory, e.g. /home/user/ComputeCpp-CE-0.1-Linux/
-
-# Useless to go on when an error occurs
-set -o errexit
 
 # How to use build.sh to compile SyclParallelSTL with ComputeCpp ?
-# ./build.sh "path/to/ComputeCpp"
+# ./build.sh "path/to/ComputeCpp" (this path can be relative)
+#
+# for example:
+#	./build.sh /home/user/ComputeCpp
 #
 # How to use build.sh to compile SyclParallelSTL with triSYCL ?
 # ./build.sh --trisycl [-DTRISYCL_INCLUDE_DIR=path/to/triSYCL/include] [-DBOOST_COMPUTE_INCLUDE_DIR=path/to/boost/compute/include]
 #
-# paths can be relative ones, this script will make them absoulte before to send them to CMake
+# for example (Ubuntu 16.04):
+#	./build.sh --trisycl -DTRISYCL_INCLUDE_DIR=~/triSYCL/include -DBOOST_COMPUTE_INCLUDE_DIR=~/compute/include
 #
-# WARNING/TODO: setting the "path/to/compute" in triSYCL mode is not an option yet
 #
+
+
+# Useless to go on when an error occurs
+set -o errexit
 
 if [ $1 == "--trisycl" ]
 then
 	shift
 	echo "build.sh enter mode: triSYCL"
-	CMAKE_ARGS="$CMAKE_ARGS -DUSE_COMPUTECPP=OFF"
+	CMAKE_ARGS="$CMAKE_ARGS -DUSE_COMPUTECPP=OFF $@"
 else
 	echo "build.sh enter mode: ComputeCpp"
 	CMAKE_ARGS="$CMAKE_ARGS -DCOMPUTECPP_PACKAGE_ROOT_DIR=$(readlink -f $1)"
@@ -46,10 +49,6 @@ function install_gmock  {(
 
 function configure  {
   mkdir -p build && pushd build
-  if [ $@ ]
-  then
-	  echo "cmake additionnal args: $@"
-  fi
   cmake .. $CMAKE_ARGS -DPARALLEL_STL_BENCHMARKS=ON $@
   popd
 }

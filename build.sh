161954cfa7f7727f42865db7e994c78a0b690d56
Ronan Keryell <ronan.keryell@xilinx.com>
2017-04-13 13:34:01 +0100

    Add display of minimal help message

--- build.sh
+++ build.sh
@@ -1,31 +1,42 @@
-#!/bin/bash
+#! /bin/bash
 
-# How to use build.sh to compile SyclParallelSTL with ComputeCpp ?
-#   ./build.sh "path/to/ComputeCpp" (this path can be relative)
-#
-# for example:
-#   ./build.sh /home/user/ComputeCpp
+function display_help() {
 
-#
-# How to use build.sh to compile SyclParallelSTL with triSYCL ?
-#   ./build.sh --trisycl [-DTRISYCL_INCLUDE_DIR=path/to/triSYCL/include] [-DBOOST_COMPUTE_INCLUDE_DIR=path/to/boost/compute/include]
-#
-# for example (Ubuntu 16.04):
-#   ./build.sh --trisycl -DTRISYCL_INCLUDE_DIR=~/triSYCL/include -DBOOST_COMPUTE_INCLUDE_DIR=~/compute/include
-#
-#
+cat <<EOT
+
+To use build.sh to compile SyclParallelSTL with ComputeCpp:
+
+  ./build.sh "path/to/ComputeCpp"
+  (the path to ComputeCpp can be relative)
+
+  For example:
+  ./build.sh /home/user/ComputeCpp
+
+
+To use build.sh to compile SyclParallelSTL with triSYCL:
+
+  ./build.sh --trisycl [-DTRISYCL_INCLUDE_DIR=path/to/triSYCL/include] [-DBOOST_COMPUTE_INCLUDE_DIR=path/to/boost/compute/include]
+
+  For example (Ubuntu 16.04):
+  ./build.sh --trisycl -DTRISYCL_INCLUDE_DIR=~/triSYCL/include -DBOOST_COMPUTE_INCLUDE_DIR=~/compute/include
+
+EOT
+}
 
 
 # Useless to go on when an error occurs
 set -o errexit
 
+# Minimal emergency case to display the help message whatever happens
+trap display_help ERR
+
 if [ $1 == "--trisycl" ]
 then
   shift
-  echo "build.sh enter mode: triSYCL"
+  echo "build.sh entering mode: triSYCL"
   CMAKE_ARGS="$CMAKE_ARGS -DUSE_COMPUTECPP=OFF $@"
 else
-  echo "build.sh enter mode: ComputeCpp"
+  echo "build.sh entering mode: ComputeCpp"
   CMAKE_ARGS="$CMAKE_ARGS -DCOMPUTECPP_PACKAGE_ROOT_DIR=$(readlink -f $1)"
   shift
 fi

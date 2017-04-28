90688f4bec33c743280e19fc18028d5c4142b5a6
Ruyman Reyes <ruyman@codeplay.com>
2017-04-28 10:53:47 +0100

    Moved travis functionality to scripts
    Added workaround for ComputeCpp builtins conflict with C99 macros.

---
+++ .travis/computecpp_workaround.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+cat .travis/additional_undef /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/sycl_builtins.h.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/sycl_builtins.h.h
+
+cat .travis/additional_undef /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/host_relational_builtins.h
+

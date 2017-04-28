e04eb5fa8b805e05ca90212e40b25ce4bedf43e3
Ruyman Reyes <ruyman@codeplay.com>
2017-04-28 13:54:45 +0100

    Extra workarounds for builtins on ComputeCpp 0.1.4

--- .travis/computecpp_workaround.sh
+++ .travis/computecpp_workaround.sh
@@ -7,3 +7,4 @@ mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/sycl_builtins.h.h
 cat .travis/additional_undef /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
 mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/host_relational_builtins.h
 
+sed -ie 's/isgreaaterequal/isgreaterequal/'  runtime/include/SYCL/host_relational_builtins.h

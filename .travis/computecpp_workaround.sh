cf489c3628f4bac52c19397a364ac0b48b67b331
Ruyman Reyes <ruyman@codeplay.com>
2017-05-12 16:18:21 +0100

    Bumped version of ComputeCpp to 0.2.0

--- .travis/computecpp_workaround.sh
+++ .travis/computecpp_workaround.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
+VERSION=0.2.0
+cat .travis/additional_undef /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/sycl_builtins.h.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/sycl_builtins.h.h
 
-cat .travis/additional_undef /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/sycl_builtins.h.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/sycl_builtins.h.h
-
-cat .travis/additional_undef /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.1.4-Linux/include/CL/host_relational_builtins.h
-
-sed -ie 's/isgreaaterequal/isgreaterequal/'  /tmp/ComputeCpp-CE-0.1.4-Linux/include/SYCL/host_relational_builtins.h
+cat .travis/additional_undef /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/host_relational_builtins.h

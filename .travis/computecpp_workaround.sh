b517d157635bf52fba32b9855e5ada0c2d2dead9
Duncan McBain <duncan@codeplay.com>
2017-05-25 19:55:28 +0100

    Update the Travis config
    Recently the Khronos ICD loader was updated, and rather than fix that
    this patch uses the standard OpenCL ICD loader and header packages.
    It also updates the config to use ComputeCpp 0.2.0, and adds a fix for
    the GCC dual ABI (the 14.04 package is built with GCC 4.9, but this
    Travis instance uses 5.something). Additionally it updates the
    ComputeCpp workaround file which had some errors.

--- .travis/computecpp_workaround.sh
+++ .travis/computecpp_workaround.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
+cat .travis/additional_undef /tmp/ComputeCpp-CE-0.2.0-Linux/include/SYCL/sycl_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.2.0-Linux/include/SYCL/sycl_builtins.h
 
-cat .travis/additional_undef /tmp/ComputeCpp-CE-0.2.0-Linux/include/CL/sycl_builtins.h.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.2.0-Linux/include/CL/sycl_builtins.h
-
-cat .travis/additional_undef /tmp/ComputeCpp-CE-0.2.0-Linux/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.2.0-Linux/include/CL/host_relational_builtins.h
-
+cat .travis/additional_undef /tmp/ComputeCpp-CE-0.2.0-Linux/include/SYCL/host_relational_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-0.2.0-Linux/include/SYCL/host_relational_builtins.h

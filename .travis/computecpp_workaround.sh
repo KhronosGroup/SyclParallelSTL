df2e81c020b2ce58b864f39bb4476d4a64827032
Ruyman Reyes <ruyman@codeplay.com>
2017-06-15 10:29:59 +0100

    Fixed: Target of ln cannot be a directory

--- .travis/computecpp_workaround.sh
+++ .travis/computecpp_workaround.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 VERSION=0.2.1
-ln -sf /tmp/ComputeCpp-CE-${VERSION}-Linux/ /tmp/ComputeCpp-latest/
 
+ln -sf /tmp/ComputeCpp-CE-${VERSION}-Linux/ /tmp/ComputeCpp-latest
 
 cat .travis/additional_undef /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h > /tmp/tmp_builtins
 mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h.h

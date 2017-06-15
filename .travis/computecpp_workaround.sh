fe3acfe21603c62be378d590cf173f2ac4e455f8
Ruyman Reyes <ruyman@codeplay.com>
2017-06-15 09:57:10 +0100

    Reducing dependencies on ComputeCpp version number

--- .travis/computecpp_workaround.sh
+++ .travis/computecpp_workaround.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 VERSION=0.2.1
-cat .travis/additional_undef /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/sycl_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/sycl_builtins.h.h
+ln -sf /tmp/ComputeCpp-CE-${VERSION}-Linux/ /tmp/ComputeCpp-latest/
 
-cat .travis/additional_undef /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-CE-${VERSION}-Linux/include/CL/host_relational_builtins.h
+
+cat .travis/additional_undef /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h.h
+
+cat .travis/additional_undef /tmp/ComputeCpp-latest/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/CL/host_relational_builtins.h
+

39394c93c76d8d7b7b191044f593b07770afd18e
Zakor <romain.biessy@codeplay.com>
2017-06-20 12:27:01 +0100

    Use given kernel name instead of wg (#34)

--- include/sycl/algorithm/buffer_algorithms.hpp
+++ include/sycl/algorithm/buffer_algorithms.hpp
@@ -192,7 +192,7 @@ B buffer_mapreduce(ExecutionPolicy &snp,
     cl::sycl::accessor<B, 1, cl::sycl::access::mode::read_write,
                        cl::sycl::access::target::local>
       sum { cl::sycl::range<1>(d.nb_work_item), cgh };
-    cgh.parallel_for_work_group<class wg>(rg, ri, [=](cl::sycl::group<1> grp) {
+    cgh.parallel_for_work_group<typename ExecutionPolicy::kernelName>(rg, ri, [=](cl::sycl::group<1> grp) {
       size_t group_id = grp.get(0);
       //assert(group_id < d.nb_work_group);
       size_t group_begin = group_id * d.size_per_work_group;
@@ -287,7 +287,7 @@ B buffer_map2reduce(ExecutionPolicy &snp,
     cl::sycl::accessor<B, 1, cl::sycl::access::mode::read_write,
                        cl::sycl::access::target::local>
       sum { cl::sycl::range<1>(d.nb_work_item), cgh };
-    cgh.parallel_for_work_group<class wg>(rng, [=](cl::sycl::group<1> grp) {
+    cgh.parallel_for_work_group<typename ExecutionPolicy::kernelName>(rng, [=](cl::sycl::group<1> grp) {
       size_t group_id = grp.get(0);
       //assert(group_id < d.nb_work_group);
       size_t group_begin = group_id * d.size_per_work_group;
@@ -395,7 +395,7 @@ void buffer_mapscan(ExecutionPolicy &snp,
                        cl::sycl::access::target::local>
       scratch { cl::sycl::range<1> { d.size_per_work_group }, cgh };
 
-    cgh.parallel_for_work_group<class wg>(rng_wg, rng_wi,
+    cgh.parallel_for_work_group<typename ExecutionPolicy::kernelName>(rng_wg, rng_wi,
                                           [=](cl::sycl::group<1> grp) {
       size_t group_id = grp.get(0);
       //assert(group_id < d.nb_work_group);
@@ -510,7 +510,7 @@ void buffer_mapscan(ExecutionPolicy &snp,
     auto read_scan = scan.template get_access
       <cl::sycl::access::mode::read>(cgh);
 
-    cgh.parallel_for_work_group<class wg>(rng_wg, rng_wi,
+    cgh.parallel_for_work_group<typename ExecutionPolicy::kernelName>(rng_wg, rng_wi,
                                           [=](cl::sycl::group<1> grp) {
       size_t group_id = grp.get(0);
       B acc = read_scan[group_id];

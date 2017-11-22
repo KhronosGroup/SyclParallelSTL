ff655f76590039962d11d5c29a0951154bc1f8f2
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-11-22 12:55:35 +0000

    Update for ComputeCpp CE 0.4.0

--- .travis/computecpp_workaround.sh
+++ .travis/computecpp_workaround.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-VERSION=0.3.2
+VERSION=0.4.0
 
-ln -sf /tmp/ComputeCpp-CE-${VERSION}-Linux/ /tmp/ComputeCpp-latest
+ln -sf /tmp/ComputeCpp-CE-${VERSION}-Ubuntu-14.04-64bit/ /tmp/ComputeCpp-latest
 
-cat .travis/additional_undef /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h.h
+cat .travis/additional_undef /tmp/ComputeCpp-latest/include/SYCL/sycl_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/SYCL/sycl_builtins.h.h
 
-cat .travis/additional_undef /tmp/ComputeCpp-latest/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
-mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/CL/host_relational_builtins.h
+cat .travis/additional_undef /tmp/ComputeCpp-latest/include/SYCL/host_relational_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/SYCL/host_relational_builtins.h
 

739adecbb48529a3ab842321f13579e0bf0f2e88
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-11-22 17:42:17 +0000

    Typo

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -12,4 +12,4 @@ sudo apt-get install boost1.58 ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
 # triSYCL requires newer cmake than in apt
 wget https://cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz
 tar -xzf /tmp/cmake.tar.gz -C /tmp
-sudo cp -rf /tmp/cmake-3.5.1-Linux-x86_64/* /tmp
+sudo cp -rf /tmp/cmake-3.5.1-Linux-x86_64/* /usr/

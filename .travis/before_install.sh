4b97ba5c20c1061558ba8676bbd3f5f133eda296
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-11-22 17:32:06 +0000

    Update cmake and compilers

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,11 +3,13 @@
 set -ev
 
 # Add modern dependencies (cmake, boost)
-sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
 sudo add-apt-repository ppa:kzemek/boost -y
 sudo apt-get update -q
 # TriSYCL requires modern boost
 # Use the system OpenCL loader
-sudo apt-get install cmake boost1.58 ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
-# Use gcc 5 as default
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+sudo apt-get install boost1.58 ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
+
+# triSYCL requires newer cmake than in apt
+wget https://cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz
+tar -xzf /tmp/cmake.tar.gz -C /tmp
+sudo cp -rf /tmp/cmake-3.5.1-Linux-x86_64/* /tmp

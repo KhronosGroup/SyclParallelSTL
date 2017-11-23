b67c59de55e8311f2c77dcb6b7ec4c3ab8f2dfb9
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-11-23 09:16:42 +0000

    Add missing boost dependencies for triSYCL

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository ppa:kzemek/boost -y
 sudo apt-get update -q
 # TriSYCL requires modern boost
 # Use the system OpenCL loader
-sudo apt-get install boost1.58 ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
+sudo apt-get install boost1.58 libboost-chrono1.58-dev libboost-log1.58-dev ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
 
 # triSYCL requires newer cmake than in apt
 wget https://cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz

cf62a59c2bd2dcf9a94a5d49eb237d63fdbfa997
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-11-23 10:25:14 +0000

    Get newer Boost.Compute & install libomp-dev

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,13 +4,17 @@ set -ev
 
 # Add modern dependencies (cmake, boost)
 sudo add-apt-repository ppa:kzemek/boost -y
-sudo add-apt-repository ppa:kylelutz/compute -y
 sudo apt-get update -q
 # TriSYCL requires modern boost
 # Use the system OpenCL loader
-sudo apt-get install boost1.58 libboost-chrono1.58-dev libboost-log1.58-dev boost-compute ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
+sudo apt-get install boost1.58 libboost-chrono1.58-dev libboost-log1.58-dev ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
 
-# triSYCL requires newer cmake than in apt
+# CMake 3.5 for triSYCL
 wget https://cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz
 tar -xzf /tmp/cmake.tar.gz -C /tmp
 sudo cp -rf /tmp/cmake-3.5.1-Linux-x86_64/* /usr/
+
+# Boost.Compute for triSYCL
+wget https://github.com/boostorg/compute/archive/master.zip -O /tmp/boost.compute.zip
+unzip /tmp/boost.compute.zip -d /tmp
+sudo cp -rf /tmp/compute-master/include /usr/

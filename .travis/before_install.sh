0f4fe2ed7363771c940106aa5dab8672453b0a1e
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-11-23 09:30:30 +0000

    Add missing boost.compute dependency for triSYCL

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -4,10 +4,11 @@ set -ev
 
 # Add modern dependencies (cmake, boost)
 sudo add-apt-repository ppa:kzemek/boost -y
+sudo add-apt-repository ppa:kylelutz/compute -y
 sudo apt-get update -q
 # TriSYCL requires modern boost
 # Use the system OpenCL loader
-sudo apt-get install boost1.58 libboost-chrono1.58-dev libboost-log1.58-dev ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
+sudo apt-get install boost1.58 libboost-chrono1.58-dev libboost-log1.58-dev boost-compute ocl-icd-libopencl1 ocl-icd-dev opencl-headers -y
 
 # triSYCL requires newer cmake than in apt
 wget https://cmake.org/files/v3.5/cmake-3.5.1-Linux-x86_64.tar.gz -O /tmp/cmake.tar.gz

f9c53f2dce63b701560d25709517f7d43738ee38
Ruyman Reyes <ruyman@codeplay.com>
2017-12-06 15:03:02 +0000

    Removing the requirement of matching ComputeCpp version

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -6,6 +6,8 @@ set -ev
 # Get ComputeCpp
 ###########################
 wget https://computecpp.codeplay.com/downloads/computecpp-ce/latest/ubuntu-14.04-64bit.tar.gz
-tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp 
+rm -rf /tmp/ComputeCpp-latest && mkdir /tmp/ComputeCpp-latest/
+tar -xzf ubuntu-14.04-64bit.tar.gz -C /tmp/ComputeCpp-latest --strip-components 1
+ls -R /tmp/ComputeCpp-latest/
 # Workaround for C99 definition conflict
 bash .travis/computecpp_workaround.sh

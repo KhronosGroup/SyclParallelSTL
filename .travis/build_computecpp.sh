b2efbe25a33e269d059f3f19c5df6599f1313bc0
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-12-08 14:07:34 +0000

    Remove the now unnecessary workaround for ComputeCpp

--- .travis/build_computecpp.sh
+++ .travis/build_computecpp.sh
@@ -9,5 +9,3 @@ wget https://computecpp.codeplay.com/downloads/computecpp-ce/latest/ubuntu-16.04
 rm -rf /tmp/ComputeCpp-latest && mkdir /tmp/ComputeCpp-latest/
 tar -xzf ubuntu-16.04-64bit.tar.gz -C /tmp/ComputeCpp-latest --strip-components 1
 ls -R /tmp/ComputeCpp-latest/
-# Workaround for C99 definition conflict
-bash .travis/computecpp_workaround.sh

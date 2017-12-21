c4470496e499c9ef5b89499e2e1b7b33192a6f6b
Toomas Remmelg <toomas.remmelg@codeplay.com>
2017-12-21 14:54:11 +0000

    Add -q to wget

--- .travis/install_intel_opencl.sh
+++ .travis/install_intel_opencl.sh
@@ -6,7 +6,7 @@ set -ev
 # Get Intel OpenCL Runtime
 ###########################
 
-wget http://registrationcenter-download.intel.com/akdlm/irc_nas/12513/opencl_runtime_16.1.2_x64_rh_6.4.0.37.tgz -O /tmp/opencl_runtime.tgz
+wget -q http://registrationcenter-download.intel.com/akdlm/irc_nas/12513/opencl_runtime_16.1.2_x64_rh_6.4.0.37.tgz -O /tmp/opencl_runtime.tgz
 tar -xzf /tmp/opencl_runtime.tgz -C /tmp
 sed 's/decline/accept/g' -i /tmp/opencl_runtime_16.1.2_x64_rh_6.4.0.37/silent.cfg
 apt-get install -yq cpio

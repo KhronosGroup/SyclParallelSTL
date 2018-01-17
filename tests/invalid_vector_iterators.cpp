c7d3019f217150ba1b8c154f311fbb6c2a70db0b
Toomas Remmelg <toomas.remmelg@codeplay.com>
2018-01-17 17:29:23 +0000

    Update copyright year to 2018

--- tests/invalid_vector_iterators.cpp
+++ tests/invalid_vector_iterators.cpp
@@ -2,7 +2,7 @@
 // parallel algorithms, and making sure that we exception or error in a
 // sensible way.
 
-/* Copyright (c) 2015 The Khronos Group Inc.
+/* Copyright (c) 2018 The Khronos Group Inc.
 
   Permission is hereby granted, free of charge, to any person obtaining a
   copy of this software and/or associated documentation files (the
